c9acad30e4b73b2e1a9fa3dc3ade5be1da375549
entity <entity@iki.fi>
2017-10-01 14:54:19 +0300

    added noderally basetrack

---
+++ assets/track.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="track" tilewidth="64" tileheight="64" tilecount="72" columns="12">
+ <image source="track.png" width="768" height="388"/>
+</tileset>
