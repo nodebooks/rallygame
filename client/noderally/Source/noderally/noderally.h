0a0a293b5243dca4881b81f970b31e67783e4814
entity <entity@iki.fi>
2018-01-28 20:34:24 +0200

    Enum for setting GUI view easily

--- client/noderally/Source/noderally/noderally.h
+++ client/noderally/Source/noderally/noderally.h
@@ -5,3 +5,16 @@
 #include "CoreMinimal.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(WebsocketLog, Log, All);
+
+/// Enum for easier UI change.
+UENUM(BlueprintType)
+enum class EUIView : uint8 
+{
+  UI_Intro              UMETA(DisplayName="Intro view"),
+  UI_Login              UMETA(DisplayName="Login view"),
+  UI_Lobby_Tracks       UMETA(Displayname="Tracks view"),
+  UI_Lobby_Races        UMETA(Displayname="Races view"),
+  UI_Lobby_ResultBoard  UMETA(DisplayName="Result Board view"),
+  UI_Waiting            UMETA(DisplayName="Waiting Race Start view"),
+  UI_Racing             UMETA(DisplayName="Racing view")
+};
